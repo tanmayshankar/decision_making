02df8fda9f7595ce6972d3bfba34b4cc1e8f519e
tanmayshankar <tanmay.shankar@gmail.com>
2016-01-18 17:18:16 +0530

    Updating multiple demo parser.

--- scripts/demonstrations/multiple_demos.py
+++ scripts/demonstrations/multiple_demos.py
@@ -15,13 +15,14 @@ from matplotlib.pyplot import *
 
 max_dist = 5
 
-action = 'd'
+action = 'e'
 discrete_space_x = 50
 discrete_space_y = 50
 
+new_demo = 'y'
+
 #Dummy set of variables for the random object spatial querying. 
 space_dist = npy.linspace(-max_dist,max_dist,discrete_space_x)
-
 
 path_plot = npy.zeros(shape=(discrete_space_x,discrete_space_y))
 
@@ -29,48 +30,95 @@ max_path_length=100
 current_pose = npy.zeros(2)
 start_pose = npy.zeros(2)
 # ax.plot_surface(X,Y,path_plot,cmap=plt.cm.jet,cstride=1,rstride=1)
-pose_train = npy.zeros(shape=(max_path_length,2))
+max_number_demos = 50
+pose_train = npy.zeros(shape=(max_number_demos,max_path_length,2))
+
+# trajectory_lengths = npy.zeros(shape=(max_number_demos,max_path_length))
+trajectory_lengths = npy.zeros(max_number_demos)
 
 state_counter = 0
 pose_train[state_counter] = start_pose
 
-while (action!='q'):
-	action = raw_input("Hit a key now: ")
-	print (action)
-	# fig = plt.figure()
-	if action=='w':
-		current_pose[0]+=1
-		state_counter+=1
-		pose_train[state_counter]=current_pose
-		# pose_train.append(current_pose)
-	if action=='a':
-		current_pose[1]-=1
-		state_counter+=1
-		pose_train[state_counter]=current_pose
-		# pose_train.append(current_pose)
-	if action=='d':
-		current_pose[1]+=1
-		state_counter+=1
-		pose_train[state_counter]=current_pose
-		# pose_train.append(current_pose)
-	if action=='s':
-		current_pose[0]-=1
-		state_counter+=1
-		pose_train[state_counter]=current_pose
-		# pose_train.append(current_pose)
+number_demos = 0
 
-	path_plot[current_pose[0]][current_pose[1]]=1
-	imshow(path_plot, interpolation='nearest', origin='lower', extent=[0,10,0,10], aspect='auto')
-	plt.show(block=False)
-	colorbar()
-	draw()
-	show() 
+while (new_demo!='n'):
+	
+	state_counter=0	
+	current_pose[0]= random.randrange(0,discrete_space_x)
+	current_pose[1]= random.randrange(0,discrete_space_y)
+	
+	# pose_train[ number_demos][state_counter][0] = current_pose[0]
+	# pose_train[number_demos][state_counter][1] = current_pose[1]
 
-trajectories = npy.zeros(shape=(state_counter+1,2))
-for i in range(0,state_counter+1):
-	trajectories[i,:]=pose_train[i,:]
+	pose_train[number_demos,state_counter,0] = current_pose[0]
+	pose_train[number_demos,state_counter,1] = current_pose[1]
+
+	path_plot[:,:]=0.
+	action='e'
+
+	while (action!='q'):	
+		# fig = plt.figure()
+		if action=='w':
+			current_pose[0]+=1
+			state_counter+=1
+			pose_train[number_demos,state_counter]=current_pose
+			# pose_train[number_demos][state_counter]=current_pose
+			# pose_train.append(current_pose)
+		if action=='a':
+			current_pose[1]-=1
+			state_counter+=1
+			pose_train[number_demos,state_counter]=current_pose
+			# pose_train[number_demos][state_counter]=current_pose
+			# pose_train.append(current_pose)
+		if action=='d':
+			current_pose[1]+=1
+			state_counter+=1
+			pose_train[number_demos,state_counter]=current_pose
+			# pose_train[number_demos][state_counter]=current_pose
+			# pose_train.append(current_pose)
+		if action=='s':
+			current_pose[0]-=1
+			state_counter+=1
+			# pose_train[number_demos][state_counter]=current_pose
+			pose_train[number_demos,state_counter]=current_pose
+			# pose_train.append(current_pose)
+
+		path_plot[current_pose[0]][current_pose[1]]=1		
+		imshow(path_plot, interpolation='nearest', origin='lower', extent=[0,10,0,10], aspect='auto')
+		plt.show(block=False)
+		colorbar()
+		draw()
+		show() 
+
+		action = raw_input("Hit a key now: ")
+		# print (action)
+		
+		trajectory_lengths[number_demos] = state_counter+1
+		# print pose_train[number_demos,:,:]
+	number_demos+=1
+	new_demo = raw_input("Do you want to start a new demonstration? ")
+	# print (new_demo)
+print "This is the pose train: ",pose_train
+
+# trajectories = npy.zeros(shape=(state_counter+1,2))
+# trajectories = npy.zeros(shape=(number_demos,state_counter+1,2))
+trajectories = npy.zeros(shape=(number_demos,state_counter+1,2))
+
+trajectory_lengths=trajectory_lengths.astype(int)
+for j in range(0,number_demos):
+	for i in range(0,trajectory_lengths[j]):
+		trajectories[j,i,:]=pose_train[j,i,:]
+		# trajectories[jet][i][:] = pose_train[j][i][:]
  # pose_train=npy.array(pose_train)
-print trajectories
+
+# trajectories=trajectories.astype(int)
+print "These are the trajectories: ",trajectories
+
+for j in range(0,number_demos):
+	# for i in range(0,trajectory_lengths[j]):
+	print "Demo:",j+1," is ",pose_train[j,:,:]
+
+
 
 with file('trajectory.txt','w') as outfile:	
 	npy.savetxt(outfile,trajectories,fmt='%-7.2f')
