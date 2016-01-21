f1c6abe5e1b854e2c03d3385e7c02aa2d1eba694
tanmayshankar <tanmay.shankar@gmail.com>
2016-01-21 18:22:14 +0530

    Updating policy iteration with argmax change.

--- scripts/search_reward/policy_iteration.py
+++ scripts/search_reward/policy_iteration.py
@@ -43,77 +43,77 @@ for i in range(0,discrete_size):
 gamma = 0.95
 
 # transition_matrix = npy.zeros(shape=(transition_space,transition_space,action_space,transition_space,transition_space))
-def alt_transition_value_2(from_state,to_state,act_index):
-	# rel_state = from_state - to_state
-	# actual_state = from_state + action_space[act_index]	
-	# act_rel_state = actual_state - to_state
+# def alt_transition_value_2(from_state,to_state,act_index):
+# 	# rel_state = from_state - to_state
+# 	# actual_state = from_state + action_space[act_index]	
+# 	# act_rel_state = actual_state - to_state
 
-	rel_state = from_state
-	rel_state[0] -= to_state[0]
-	rel_state[1] -= to_state[1]
+# 	rel_state = from_state
+# 	rel_state[0] -= to_state[0]
+# 	rel_state[1] -= to_state[1]
 
-	actual_state = from_state
-	actual_state[0] += action_space[act_index][0]
-	actual_state[1] += action_space[act_index][1]
+# 	actual_state = from_state
+# 	actual_state[0] += action_space[act_index][0]
+# 	actual_state[1] += action_space[act_index][1]
 
-	act_rel_state = actual_state
-	act_rel_state[0] -=to_state[0]
-	act_rel_state[1] -=to_state[1]
+# 	act_rel_state = actual_state
+# 	act_rel_state[0] -=to_state[0]
+# 	act_rel_state[1] -=to_state[1]
 
-	if (npy.linalg.norm(rel_state)>2):
-		return 0.
-	else:
-		if (rel_state==actual_state):
-			return 0.7
-		if (npy.linalg.norm(rel_state)==0):
-			return 0.1
-		if (npy.linalg.norm(action_space[act_index])==1):
-			if (rel_state==(from_state-action_space[act_index])or(npy.linalg.norm(rel_state)>1)):
-				return 0.
-			else: 
-				return 0.1
-		else: 
-			if (npy.linalg.norm(rel_state)==1):
-				return 0.1
-			else:
-				return 0.
+# 	if (npy.linalg.norm(rel_state)>2):
+# 		return 0.
+# 	else:
+# 		if (rel_state==actual_state):
+# 			return 0.7
+# 		if (npy.linalg.norm(rel_state)==0):
+# 			return 0.1
+# 		if (npy.linalg.norm(action_space[act_index])==1):
+# 			if (rel_state==(from_state-action_space[act_index])or(npy.linalg.norm(rel_state)>1)):
+# 				return 0.
+# 			else: 
+# 				return 0.1
+# 		else: 
+# 			if (npy.linalg.norm(rel_state)==1):
+# 				return 0.1
+# 			else:
+# 				return 0.
 
-def alt_transition_value(from_state,to_state,act_index):
-	# rel_state = from_state - to_state
-	# actual_state = from_state + action_space[act_index]	
-	# act_rel_state = actual_state - to_state
+# def alt_transition_value(from_state,to_state,act_index):
+# 	# rel_state = from_state - to_state
+# 	# actual_state = from_state + action_space[act_index]	
+# 	# act_rel_state = actual_state - to_state
 
-	# return 1./9
-	rel_state = from_state
-	rel_state[0] -= to_state[0]
-	rel_state[1] -= to_state[1]
+# 	# return 1./9
+# 	rel_state = from_state
+# 	rel_state[0] -= to_state[0]
+# 	rel_state[1] -= to_state[1]
 
-	actual_state = from_state
-	actual_state[0] += action_space[act_index][0]
-	actual_state[1] += action_space[act_index][1]
+# 	actual_state = from_state
+# 	actual_state[0] += action_space[act_index][0]
+# 	actual_state[1] += action_space[act_index][1]
 
-	act_rel_state = actual_state
-	act_rel_state[0] -=to_state[0]
-	act_rel_state[1] -=to_state[1]
+# 	act_rel_state = actual_state
+# 	act_rel_state[0] -=to_state[0]
+# 	act_rel_state[1] -=to_state[1]
 
-	opp_state = from_state
-	opp_state[0] -= action_space[act_index][0]
-	opp_state[1] -= action_space[act_index][1]
+# 	opp_state = from_state
+# 	opp_state[0] -= action_space[act_index][0]
+# 	opp_state[1] -= action_space[act_index][1]
 	
-	if (to_state==actual_state):
-		return 0.7
-	if (npy.linalg.norm(rel_state)==0.):
-		return 0.1
-	if (npy.linalg.norm(action_space[act_index])==1.):
-		if ((rel_state==opp_state)or(npy.linalg.norm(rel_state)>1.)):		
-			return 0.
-		else: 
-			return 0.1
-	else: 
-		if (npy.linalg.norm(rel_state)==1):
-			return 0.1
-		else:
-			return 0.
+# 	if (to_state==actual_state):
+# 		return 0.7
+# 	if (npy.linalg.norm(rel_state)==0.):
+# 		return 0.1
+# 	if (npy.linalg.norm(action_space[act_index])==1.):
+# 		if ((rel_state==opp_state)or(npy.linalg.norm(rel_state)>1.)):		
+# 			return 0.
+# 		else: 
+# 			return 0.1
+# 	else: 
+# 		if (npy.linalg.norm(rel_state)==1):
+# 			return 0.1
+# 		else:
+# 			return 0.
 
 # def transition_value(from_state,to_state,act_index):
 def transition_value(to_state_i,to_state_j,act_index):
@@ -147,9 +147,10 @@ def policy_iteration():
 			for from_state_j in range(1,discrete_size-1):
 				# value_functions[t+1,state_i,state_j] = 				
 				# print "For state:",from_state_i,from_state_j
+				value_per_action[:]=0.
+				max_val = 0.
 				for act in range(0,action_size):
-					value_per_action[:]=0.
-					max_val = 0.
+					
 					# print "For action:",act
 					for to_state_i in range(-1,2):
 						for to_state_j in range(-1,2):
@@ -164,17 +165,22 @@ def policy_iteration():
 								# print "Bahahahhahahahahahaha",trans_prob							
 							#BELLMAN UPDATE EQUATION - part 1. 
 							value_per_action[act] += trans_prob*(reward_function[to_state[0],to_state[1]] + gamma*value_functions[t,to_state[0],to_state[1]])
-					# print act,value_per_action[act]
+
+					
 					if (value_per_action[act]>max_val):
 						max_val = value_per_action[act]
 						optimal_policy[from_state_i,from_state_j] = act
-						# print "Optimal action:",act
+						value_functions[t+1,from_state_i,from_state_j] = max_val
+					
 	for i in range(0,discrete_size):
 		# for j in range(0,discrete_size):
 		print optimal_policy[i]
-
 
 for i in range(0,discrete_size):
 	print reward_function[i]
 
 policy_iteration()
+
+print "These are the value functions."
+for t in range(0,time_limit):
+	print value_functions[t]
